093a7df11c23de1cd9c66b051e887466e031e622
himanshu goyal <hiteshgoyal943gmail.com>
2024-03-30 01:33:45 +0530

    add side summaru

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,62 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
   const [videoId, setVideoId] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [summary, setSummary] = useState("");
 
   useEffect(() => {
     const getVideoId = async () => {
       let [tab] = await chrome.tabs.query({ active: true });
       const id = tab.url.split("v=")[1].split("&")[0];
+      console.log(id);
       setVideoId(id);
     };
     getVideoId();
   }, []);
 
+  const fetchData = async () => {
+    const url = `https://0cbzh110-5000.inc1.devtunnels.ms/summary/${videoId}`;
+    console.log(url);
+    try {
+      setLoading(true);
+      const response = await axios.get(url);
+      const { data } = response;
+      setSummary(data.summary);
+      setLoading(false);
+      chrome.storage.local.set({
+        summary: data.summary,
+      });
+      doSomething();
+    } catch (error) {
+      console.error("Error fetching video details:", error);
+      setSummary("Error fetching video details");
+      setLoading(false);
+    }
+  };
+
+  // useEffect(() => {
+  //   doSomething();
+  // }, [summary]);
+
+  const doSomething = async () => {
+    let [tab] = await chrome.tabs.query({ active: true });
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: () => {
+        chrome.storage.local.get(["summary"], (res) => {
+          document.getElementById("secondary").textContent = res.summary;
+          document.getElementById("contents").textContent = "hllo";
+        });
+      },
+    });
+  };
+
   // const id = "1";
+  if (loading) return <h1>Loading...</h1>;
+
   return (
     <>
       <h1 className="flex flex-col items-center font-bold text-4xl">
@@ -26,17 +67,33 @@ function App() {
         />
         Study Buddy{" "}
       </h1>
+      {summary && <p>{summary}</p>}
+
       <button
-        onClick={() =>
-          videoId != ""
-            ? window.open(`http://localhost:5173/video/${videoId}/0`, "_blank")
-            : alert("Please open a youtube video")
+        onClick={
+          () => fetchData()
+          // videoId != ""
+          //   ? window.open(`http://localhost:5173/video/${videoId}/0`, "_blank")
+          //   : alert("Please open a youtube video")
         }
         className="bg-black mt-4
       focus:outline-none
       text-white border-none outline-none"
       >
         Get Help With this Video
+      </button>
+      <button
+        onClick={
+          () => doSomething()
+          // videoId != ""
+          //   ? window.open(`http://localhost:5173/video/${videoId}/0`, "_blank")
+          //   : alert("Please open a youtube video")
+        }
+        className="bg-black mt-4
+      focus:outline-none
+      text-white border-none outline-none"
+      >
+        storage
       </button>
     </>
   );
